69edd695484ee6f3eb29d04be89648232f0b757c
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-03 00:59:23 +0530

    Loading

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ document.getElementById('body-container').style.display = 'none';
 $(document).ready(function() {
     var counter = 0;
     var i = setInterval(function(){
-      $("#preloader .container div #count").html(counter);
+      $("#preloader .container #wrapper2 #count").html(counter);
       counter++;
       if(counter == 101){
         clearInterval(i);

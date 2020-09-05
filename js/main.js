5045fbba2ede610180a687674df58d5a2a5ed0db
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-05 17:32:29 +0530

    Portfolio

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@
 //     window.loaded = true;
 // });
 
-var colc = new Colcade( '.grid', {
+$('.grid').colcade({
   columns: '.grid-col',
   items: '.grid-item'
-});
-
+})
 
 //Preloader and page fade in setup
 function fadeInChildren(parent, base_delay) {

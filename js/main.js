938962810e477eb95fc67a0dc841defb3272d436
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-07 14:23:49 +0530

    Portfolio details

--- js/main.js
+++ js/main.js
@@ -85,20 +85,20 @@ $(function() {
     var maxHeight = '';
     var heights = [];
 
-    $.each($('#portfolioDetails .container .row #myCarousel .carousel-inner .item'), function(item){
-      heights.push(item.outerHeight(true));
+    $(".carousel-inner > .item").siblings().each(function(i){
+      heights.push($(this).outerHeight());
     });
 
     maxHeight = Math.max.apply(Math, heights);
 
-    $.each($('#portfolioDetails .container .row #myCarousel .carousel-inner .item'), function(item) {
-      item.css('height', maxHeight + 'px');
+    $(".carousel-inner > .item").siblings().each(function(i){
+      $(this).css('height', maxHeight + 'px');
     });
 
-    var y_shift = '';
-    y_shift =  ($('#portfolioDetails .container .row #myCarousel').outerHeight(true) - (maxHeight + 'px'))/2;
-    console.log($('#portfolioDetails .container .row #myCarousel').outerHeight(true), (maxHeight + 'px'), y_shift);
-    $('#portfolioDetails .container .row #myCarousel .carousel-inner .item').css('background-position-y') = y_shift;
+    // var y_shift = '';
+    // y_shift =  ($('#portfolioDetails .container .row #myCarousel').outerHeight(true) - (maxHeight + 'px'))/2;
+    // console.log($('#portfolioDetails .container .row #myCarousel').outerHeight(true), (maxHeight + 'px'), y_shift);
+    // $('#portfolioDetails .container .row #myCarousel .carousel-inner .item').css('background-position-y') = y_shift;
 });
 
 

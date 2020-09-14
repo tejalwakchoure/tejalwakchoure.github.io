26b2e9c9bdcb4d045c4b45e34ebb46747bdb930f
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-15 02:00:56 +0530

    carousel

--- js/main.js
+++ js/main.js
@@ -149,6 +149,11 @@ var mySwiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
   loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',

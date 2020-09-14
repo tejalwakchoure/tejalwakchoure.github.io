14099a306c5ab16064497860f60e57dbb7734ea1
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-15 01:02:37 +0530

    carousel

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ function toggleDetails(num) {
         console.log('collapse toggled show/hide')
     }
     // else if($('#portfolioDetails').is('.collapse:not(.show)')) {
-    else if($('#portfolioDetails').is( ":hidden") {
+    else if($('#portfolioDetails').is( ":hidden")) {
         console.log("#slide-", num, " is not active ; section is not visible")
         $('#portfolioDetails').collapse('toggle');
         console.log('collapse toggled to show')

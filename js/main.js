4b52614dac13c3c283a54ed18989be88714acbd4
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-18 01:01:09 +0530

    carousel

--- js/main.js
+++ js/main.js
@@ -30,8 +30,10 @@ $(document).ready(function() {
         $('.hide1 header .img-responsive').css({opacity: 0.0, visibility: "visible"}).delay(500).animate({opacity: 1.0}, 500);
         $('.hide1 header').css({opacity: 0.0, visibility: "visible"}).delay(500).animate({opacity: 1.0}, 500);  
         fadeInChildren('.hide1 header .intro-text',1000);
-        $('.hide2').css({opacity: 0.0, visibility: "visible"}).delay(2300).animate({opacity: 1.0}, 500);
-        $('#portfolioDetails .container .row #myCarousel .carousel-inner #slide-6').addClass('active');
+        $('.hide1').css({opacity: 0.0, visibility: "visible"}).delay(500).animate({opacity: 1.0}, 500);  
+        // fadeInChildren('.hide1 section',1000);
+        // $('.hide2').css({opacity: 0.0, visibility: "visible"}).delay(2300).animate({opacity: 1.0}, 500);
+        // $('#portfolioDetails .container .row #myCarousel .carousel-inner #slide-6').addClass('active');
         startTime();
      }
     }, 10);

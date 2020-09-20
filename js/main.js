350fce4ccbeb391935bddfa6ecbbd89ee4b2f2f9
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-21 01:40:47 +0530

    animation

--- js/main.js
+++ js/main.js
@@ -50,6 +50,32 @@ $(document).ready(function() {
 //  });
 
 
+
+$(window).scroll(function() {
+  var $window = $(window),
+  $body = $('.body-container'),
+  $panel = $('.section-bkg');
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+
+  $panel.each(function () {
+    var $this = $(this);
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
+
+
+
+
 // local datetime update
 function startTime() {
     var today = new Date();

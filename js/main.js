e8de2a065d3917a48a7d69cfe4a0a546ea0b2b04
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-09-29 01:58:26 +0530

    scroll

--- js/main.js
+++ js/main.js
@@ -122,8 +122,8 @@ function toggleOnPortfolioLink(num) {
         $('#portfolio').show().siblings('section').hide();
         num = 6 - num;
         // $(this).attr('href')="#portfolio-item-"+num;
-        document.getElementById("#portfolio-item-"+num).scrollIntoView(true);
-        // document.getElementById("#slide-link-"+num).href="#portfolio-item-"+num;
+        document.getElementById("portfolio-item-"+num).scrollIntoView(true);
+        // document.getElementById("slide-link-"+num).href="#portfolio-item-"+num;
     }
     else if($('#portfolio').css('display')!='none') {
         $('#portfolioDetails').show().siblings('section').hide();

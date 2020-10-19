1fba05727c1f1d97d83a08b41cdc6099c16ddf43
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-10-20 02:21:44 +0530

    fonts

--- js/main.js
+++ js/main.js
@@ -315,20 +315,22 @@ $('.navbar-collapse ul li a').click(function() {
 
 
 // Change brand properties when in/out of Header
-$(window).on("load resize scroll", function() {
-  window.loaded = true;
-  // if($('header').position().top <= $(window).scrollTop() && ($('header').position().top + $('header').outerHeight()) > $(window).scrollTop()) {
-  if($('header').offset().top <= $(window).scrollTop() && ($('header').offset().top + $('header').outerHeight()) > $(window).scrollTop()) {
-     $('.sidenav .page-scroll .navbar-brand').css({
-            'font-size' : '4.5em',
-            'margin' : '50px 10px 40px 0px'
-        });
-  } else {
-    $('.sidenav .page-scroll .navbar-brand').css({
-            'font-size' : '2em',
-            'margin' : '0'
-        });
-  }
+// $(window).on("load resize scroll", function() {
+//   window.loaded = true;
+//   // if($('header').position().top <= $(window).scrollTop() && ($('header').position().top + $('header').outerHeight()) > $(window).scrollTop()) {
+//   if($('header').offset().top <= $(window).scrollTop() && ($('header').offset().top + $('header').outerHeight()) > $(window).scrollTop()) {
+//      $('.sidenav .page-scroll .navbar-brand').css({
+//             'font-size' : '4.5em',
+//             'margin' : '50px 10px 40px 0px'
+//         });
+//   } else {
+//     $('.sidenav .page-scroll .navbar-brand').css({
+//             'font-size' : '2em',
+//             'margin' : '0'
+//         });
+//   }
+
+
 // });
 
 // Horizontal header parallax

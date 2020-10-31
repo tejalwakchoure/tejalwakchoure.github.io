678e6c1f9c8e58303ec0e7fa3339675649741848
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-11-01 01:09:30 +0530

    technical changes

--- js/main.js
+++ js/main.js
@@ -344,17 +344,3 @@ $(window).on("load resize scroll", function() {
       $('.hor_parallax_left').css({ left: pos });
 });
 
-
-(function () {
-    if (typeof EventTarget !== "undefined") {
-        let func = EventTarget.prototype.addEventListener;
-        EventTarget.prototype.addEventListener = function (type, fn, capture) {
-            this.func = func;
-            if(typeof capture !== "boolean"){
-                capture = capture || {};
-                capture.passive = false;
-            }
-            this.func(type, fn, capture);
-        };
-    };
-}());

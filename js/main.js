a97c46f8a150a5eeee9e2777b8ab7bb3cecc71d1
tejalwakchoure <tejal.wakchoure@gmail.com>
2021-04-27 19:15:22 +0530

    cross-browser compatibility

--- js/main.js
+++ js/main.js
@@ -58,8 +58,8 @@ $(document).ready(function() {
   // } else {
 
    $('body').niceScroll({
-        scrollspeed: 80, // scrolling speed - moves faster if speed is less
-        mousescrollstep: 80, // scrolling speed with mouse wheel (pixel)
+        scrollspeed: 40, // scrolling speed - moves faster if speed is less
+        mousescrollstep: 40, // scrolling speed with mouse wheel (pixel)
         autohidemode: false, // how hide the scrollbar works
         //    scrollspeed:60,
         //    mousescrollstep:8*3,
@@ -70,13 +70,13 @@ $(document).ready(function() {
   
 });
 
-$('body').scroll(function(){
-  $('body').getNiceScroll().resize();
-});
-
-// $('body').mouseover(function(){
+// $('body').scroll(function(){
 //   $('body').getNiceScroll().resize();
 // });
+
+$('body').mouseover(function(){
+  $('body').getNiceScroll().resize();
+});
 
 
 

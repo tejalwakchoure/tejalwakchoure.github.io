d696844453a2df48c931f0d24781327992637338
tejalwakchoure <tejal.wakchoure@gmail.com>
2021-05-09 17:42:21 +0530

    cross-browser compatibility

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,37 +1,48 @@
-const SmoothScroll = (target, speed, smooth) => {
+function SmoothScroll(target, speed, smooth) {
   if (target === document)
-    target = (document.documentElement || document.body.parentNode || document.body) // cross browser support for document scrolling
-
-  let moving = false;
-  let pos = target.scrollTop;
-  target.addEventListener('mousewheel', scrolled, false)
-  /* target.addEventListener('DOMMouseScroll', scrolled, false) */
+    target = (document.scrollingElement 
+              || document.documentElement 
+              || document.body.parentNode 
+              || document.body) // cross browser support for document scrolling
+      
+  var moving = false
+  var pos = target.scrollTop
+  var frame = target === document.body 
+              && document.documentElement 
+              ? document.documentElement 
+              : target // safari is the new IE
+  
+  target.addEventListener('mousewheel', scrolled, { passive: false })
+  target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
 
   function scrolled(e) {
     e.preventDefault(); // disable default scrolling
-    let delta = e.delta || e.wheelDelta;
-    if (delta === undefined) {
-      //we are on firefox
-      delta = -e.detail;
-    }
-    delta = Math.max(-1, Math.min(1, delta)) // cap the delta to [-1,1] for cross browser consistency
+
+    var delta = normalizeWheelDelta(e)
 
     pos += -delta * speed
-    pos = Math.max(0, Math.min(pos, (target.scrollHeight - target.clientHeight) + (smooth * 2))) // limit scrolling
+    pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
 
     if (!moving) update()
   }
 
+  function normalizeWheelDelta(e){
+    if(e.detail){
+      if(e.wheelDelta)
+        return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
+      else
+        return -e.detail/3 // Firefox
+    }else
+      return e.wheelDelta/120 // IE,Safari,Chrome
+  }
+
   function update() {
     moving = true
-    let delta = (pos - target.scrollTop) / smooth;
-
-    delta -= 1;
-
-    if(pos - target.scrollTop === smooth * 2)
-      delta = 0;
-
+    
+    var delta = (pos - target.scrollTop) / smooth
+    
     target.scrollTop += delta
+    
     if (Math.abs(delta) > 0.5)
       requestFrame(update)
     else

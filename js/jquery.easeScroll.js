8d330862a0aa19585c4ff73b71099b6adca556fe
tejalwakchoure <tejal.wakchoure@gmail.com>
2021-05-09 20:13:14 +0530

    cross-browser compatibility

--- js/jquery.easeScroll.js
+++ js/jquery.easeScroll.js
@@ -258,7 +258,8 @@ $.fn.easeScroll = function(options) {
                 }
             }(),
             // K = /chrome|iPad/i.test(window.navigator.userAgent),
-            L = "onwheel" in document ? "wheel" : ((document.onmousewheel !== undefined) ? "mousewheel" : "DOMMouseScroll");
+            // L = "onwheel" in document ? "wheel" : ((document.onmousewheel !== undefined) ? "mousewheel" : "DOMMouseScroll");
+            L = ((document.onmousewheel !== undefined) ? "mousewheel" : "DOMMouseScroll");
             (u("mousedown", a, {passive:false}), u(L, n, {passive:false}), u("load", t, {passive:false}))
     }();
 }

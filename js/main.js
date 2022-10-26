e1e19fba5ca744c65bb365826f21020d35f0a1c1
tejalwakchoure <tejal.wakchoure@gmail.com>
2022-10-27 02:42:14 +0530

    update post pages

--- js/main.js
+++ js/main.js
@@ -295,3 +295,23 @@ $(window).on("load", function() {
 $(document).ready(function() {
   $('#portfolio .portfolio-link-wrapper').css({visibility: "visible"});
 });
+
+
+// Post back link
+var element = document.getElementById('back-link');
+
+// Provide a standard href to facilitate standard browser features such as 
+//  - Hover to see link
+//  - Right click and copy link
+//  - Right click and open in new tab
+element.setAttribute('href', document.referrer);
+
+// We can't let the browser use the above href for navigation. If it does, 
+// the browser will think that it is a regular link, and place the current 
+// page on the browser history, so that if the user clicks "back" again,
+// it'll actually return to this page. We need to perform a native back to
+// integrate properly into the browser's history behavior
+element.onclick = function() {
+  history.back();
+  return false;
+}

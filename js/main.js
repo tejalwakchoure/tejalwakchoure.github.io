5e981648eb68e080e763f753fad0b97d191593b7
tejalwakchoure <tejal.wakchoure@gmail.com>
2022-10-26 16:21:46 +0530

    update nav and footer

--- js/main.js
+++ js/main.js
@@ -66,41 +66,41 @@ $(document).ready(function() {
 });
 
 // Portfolio parallax
-function addPortfolioParallax() {
-    var startParallaxTime = Date.now();
-    var interval = setInterval(function () {
-        if ($('#body-container').is(':visible') || Date.now() - startParallaxTime > 4000) {
-            clearInterval(interval);
-            var images = document.querySelectorAll('.px_img');
-            images.forEach(img => {
-                var img_parallax = new simpleParallax(img, {delay: .5,
-                                                            scale: 1.3,
-                                                            transition: 'cubic-bezier(0,0,0,1)',
-                                                            customWrapper: img.parentNode.nodeName,
-                                                            });
-            });
-        }
-    }, 100);
-}
+// function addPortfolioParallax() {
+//     var startParallaxTime = Date.now();
+//     var interval = setInterval(function () {
+//         if ($('#body-container').is(':visible') || Date.now() - startParallaxTime > 4000) {
+//             clearInterval(interval);
+//             var images = document.querySelectorAll('.px_img');
+//             images.forEach(img => {
+//                 var img_parallax = new simpleParallax(img, {delay: .5,
+//                                                             scale: 1.3,
+//                                                             transition: 'cubic-bezier(0,0,0,1)',
+//                                                             customWrapper: img.parentNode.nodeName,
+//                                                             });
+//             });
+//         }
+//     }, 100);
+// }
 
 // Local datetime update
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var mth = today.getMonth() + 1;
-    var day = today.getDate();
-    h = checkTime(h);
-    m = checkTime(m);
-    mth = checkTime(mth);
-    day = checkTime(day);
-    $('.navbar-text').html(day + "/" + mth  +  "<br>" + h + ":" + m);
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
-}
+// function startTime() {
+//     var today = new Date();
+//     var h = today.getHours();
+//     var m = today.getMinutes();
+//     var mth = today.getMonth() + 1;
+//     var day = today.getDate();
+//     h = checkTime(h);
+//     m = checkTime(m);
+//     mth = checkTime(mth);
+//     day = checkTime(day);
+//     $('.navbar-text').html(day + "/" + mth  +  "<br>" + h + ":" + m);
+//     var t = setTimeout(startTime, 500);
+// }
+// function checkTime(i) {
+//     if (i < 10) {i = "0" + i};
+//     return i;
+// }
 
 // Preload images & background images
 function preloadImages(dir, imageArray, index=0) {
@@ -114,125 +114,125 @@ function preloadImages(dir, imageArray, index=0) {
 }
 
 // Swiper initializations
-var interleaveOffset = 0.5;
-var interleaveSpeed = 1000;
+// var interleaveOffset = 0.5;
+// var interleaveSpeed = 1000;
 
-var swiper_innermost = new Swiper('.swiper-container-innermost', {
-      observer: true,
-      observeParents: true,
-      speed: interleaveSpeed,
-      watchSlidesProgress: true,
-      on: {
-        progress: function() {
-          var swiper = this;
-          $('.swiper-container-innermost .slide-inner').each(function() { 
-            var slideProgress = $(this).progress;
-            var innerOffset = swiper.width * interleaveOffset;
-            var innerTranslate = slideProgress * innerOffset;
-            $(this).css({"-ms-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
-            $(this).css({"-webkit-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
-            $(this).css({"transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
-          })
-        },
-        touchStart: function() {
-          $('.swiper-container-innermost .swiper-slide').each(function() { 
-            $(this).css({"transition" :  ""});
-          })
-        },
-        setTransition: function(speed) {
-          $('.swiper-container-innermost .swiper-slide').each(function() { 
-            $(this).css({"transition" :  speed + "ms"});
-          });
-          $('.swiper-container-innermost .slide-inner').each(function() { 
-            $(this).css({"transition" :  speed + "ms"});
-          });
-        }
-      }
-    });
+// var swiper_innermost = new Swiper('.swiper-container-innermost', {
+//       observer: true,
+//       observeParents: true,
+//       speed: interleaveSpeed,
+//       watchSlidesProgress: true,
+//       on: {
+//         progress: function() {
+//           var swiper = this;
+//           $('.swiper-container-innermost .slide-inner').each(function() { 
+//             var slideProgress = $(this).progress;
+//             var innerOffset = swiper.width * interleaveOffset;
+//             var innerTranslate = slideProgress * innerOffset;
+//             $(this).css({"-ms-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
+//             $(this).css({"-webkit-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
+//             $(this).css({"transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
+//           })
+//         },
+//         touchStart: function() {
+//           $('.swiper-container-innermost .swiper-slide').each(function() { 
+//             $(this).css({"transition" :  ""});
+//           })
+//         },
+//         setTransition: function(speed) {
+//           $('.swiper-container-innermost .swiper-slide').each(function() { 
+//             $(this).css({"transition" :  speed + "ms"});
+//           });
+//           $('.swiper-container-innermost .slide-inner').each(function() { 
+//             $(this).css({"transition" :  speed + "ms"});
+//           });
+//         }
+//       }
+//     });
 
-var swiper_in = new Swiper('.swiper-container-in', {
-      observer: true,
-      observeParents: true,
-      speed: interleaveSpeed,
-      watchSlidesProgress: true,
-      on: {
-        progress: function() {
-          var swiper = this;
-          $('.swiper-container-in .slide-inner').each(function() { 
-            var slideProgress = $(this).progress;
-            var innerOffset = swiper.width * interleaveOffset;
-            var innerTranslate = slideProgress * innerOffset;
-            $(this).css({"-ms-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
-            $(this).css({"-webkit-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
-            $(this).css({"transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
-          })
-        },
-        touchStart: function() {
-          $('.swiper-container-in .swiper-slide').each(function() { 
-            $(this).css({"transition" :  ""});
-          })
-        },
-        setTransition: function(speed) {
-          $('.swiper-container-in .swiper-slide').each(function() { 
-            $(this).css({"transition" :  speed + "ms"});
-          });
-          $('.swiper-container-in .slide-inner').each(function() { 
-            $(this).css({"transition" :  speed + "ms"});
-          });
-        }
-      }
-    });
+// var swiper_in = new Swiper('.swiper-container-in', {
+//       observer: true,
+//       observeParents: true,
+//       speed: interleaveSpeed,
+//       watchSlidesProgress: true,
+//       on: {
+//         progress: function() {
+//           var swiper = this;
+//           $('.swiper-container-in .slide-inner').each(function() { 
+//             var slideProgress = $(this).progress;
+//             var innerOffset = swiper.width * interleaveOffset;
+//             var innerTranslate = slideProgress * innerOffset;
+//             $(this).css({"-ms-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
+//             $(this).css({"-webkit-transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
+//             $(this).css({"transform" :  "translate3d(" + innerTranslate + "px, 0, 0)"});
+//           })
+//         },
+//         touchStart: function() {
+//           $('.swiper-container-in .swiper-slide').each(function() { 
+//             $(this).css({"transition" :  ""});
+//           })
+//         },
+//         setTransition: function(speed) {
+//           $('.swiper-container-in .swiper-slide').each(function() { 
+//             $(this).css({"transition" :  speed + "ms"});
+//           });
+//           $('.swiper-container-in .slide-inner').each(function() { 
+//             $(this).css({"transition" :  speed + "ms"});
+//           });
+//         }
+//       }
+//     });
 
-var swiper = new Swiper('.swiper-container', {
-      // cssMode: true,
-      // virtualTranslate: true,
-      touchEventsTarget: 'wrapper',
-      simulateTouch: false,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      mousewheel: {
-        forceToAxis: true,
-        sensitivity: 3,
-        thresholdDelta: 3,
-      },   
-      keyboard: true,
-      observer: true,
-      observeParents: true,
-      watchSlidesProgress: true,
-    });
+// var swiper = new Swiper('.swiper-container', {
+//       // cssMode: true,
+//       // virtualTranslate: true,
+//       touchEventsTarget: 'wrapper',
+//       simulateTouch: false,
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//       },
+//       mousewheel: {
+//         forceToAxis: true,
+//         sensitivity: 3,
+//         thresholdDelta: 3,
+//       },   
+//       keyboard: true,
+//       observer: true,
+//       observeParents: true,
+//       watchSlidesProgress: true,
+//     });
 
 // Toggle visibility of project carousel
-function toggleOnPortfolioLink(num) {
-    if(num == -1) {
-      num = swiper.realIndex;
-    }
-    if($('#portfolioDetails').css('display')!='none') {
-        $('#portfolio').addClass("animate__animated animate__slideInLeft");
-        $('#portfolio').show().siblings('section').hide();
-        num = 6 - num;
-        document.getElementById("portfolio-item-"+num).scrollIntoView(true);
-    }
-    else if($('#portfolio').css('display')!='none') {
-        $('#portfolioDetails').css({display: 'table'});
-        $('#portfolioDetails').show().siblings('section').hide();
-        swiper.update();
-        swiper.slideTo(num);
-        swiper_in.update();
-        swiper_in.slideTo(num);
-        swiper_innermost.update();
-        swiper_innermost.slideTo(num);
-        document.getElementById('work').scrollIntoView(true);
-    }
-    else {
-    }
-    return false;
-}
+// function toggleOnPortfolioLink(num) {
+//     if(num == -1) {
+//       num = swiper.realIndex;
+//     }
+//     if($('#portfolioDetails').css('display')!='none') {
+//         $('#portfolio').addClass("animate__animated animate__slideInLeft");
+//         $('#portfolio').show().siblings('section').hide();
+//         num = 6 - num;
+//         document.getElementById("portfolio-item-"+num).scrollIntoView(true);
+//     }
+//     else if($('#portfolio').css('display')!='none') {
+//         $('#portfolioDetails').css({display: 'table'});
+//         $('#portfolioDetails').show().siblings('section').hide();
+//         swiper.update();
+//         swiper.slideTo(num);
+//         swiper_in.update();
+//         swiper_in.slideTo(num);
+//         swiper_innermost.update();
+//         swiper_innermost.slideTo(num);
+//         document.getElementById('work').scrollIntoView(true);
+//     }
+//     else {
+//     }
+//     return false;
+// }
 
 // onclick page scrolling - requires jQuery Easing plugin
 // $(function() {
@@ -252,15 +252,16 @@ function toggleOnPortfolioLink(num) {
 // });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+// $('body').scrollspy({
+//     target: '.navbar-fixed-top'
+// })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+// $('.navbar-collapse ul li a').click(function() {
+//     $('.navbar-toggle:visible').click();
+// });
 
+// Preload images
 $(window).on("load", function() {
   var smallScreen = $(window).width() < 768 ? true : false;
   var dir1 = smallScreen ? 'assets/img/small-devices/' : 'assets/img/';
@@ -273,18 +274,18 @@ $(window).on("load", function() {
 });
 
 // Horizontal header parallax
-$(window).on("load resize scroll", function() {
-  if($('header').length) {
-    // Depending on the browser, you may need to use var windowTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    var windowTop = $(window).scrollTop() || 0;
-    var elementTop = $('header').offset().top || 0;
-    var currentpos = -10;
-    var pos = currentpos + ((windowTop - elementTop) / 10);
+// $(window).on("load resize scroll", function() {
+//   if($('header').length) {
+//     // Depending on the browser, you may need to use var windowTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+//     var windowTop = $(window).scrollTop() || 0;
+//     var elementTop = $('header').offset().top || 0;
+//     var currentpos = -10;
+//     var pos = currentpos + ((windowTop - elementTop) / 10);
 
-    if (pos < -10) {
-        pos = -10;
-    }
-    $('.hor_parallax_right').css({ right: pos });
-    $('.hor_parallax_left').css({ left: pos });
-  }
-});
+//     if (pos < -10) {
+//         pos = -10;
+//     }
+//     $('.hor_parallax_right').css({ right: pos });
+//     $('.hor_parallax_left').css({ left: pos });
+//   }
+// });
